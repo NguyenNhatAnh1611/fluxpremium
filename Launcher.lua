c44628d7f593a744f447639c40f4cdb69237721e
NguyenNhatAnh1611 <110982409+NguyenNhatAnh1611@users.noreply.github.com>
2022-09-20 08:32:22 +0700

    Update Launcher.lua

--- Launcher.lua
+++ Launcher.lua
@@ -1,11 +1,10 @@
+local HttpService = game:GetService"HttpService"
 local Response = syn.request({
     Url = "https://fluxdatabase.nguyennhatanh16.repl.co/verify",
     Method = "POST",
     Headers = {
         ["Content-Type"] = "application/json"
     }, 
-    Body = {
-        ['key'] = _G.Key
-    }
+    Body = HttpService:JSONEncode({['key'] = _G.key} )
 })
 print(Response.Body)
